d339928c95ce8465348f948724cc0ab76da4e403
Arya <aryatahery@gmail.com>
2023-11-15 00:16:58 +0330

    add executer function

--- Program.cs
+++ Program.cs
@@ -1,14 +1,23 @@
 ﻿// Coded by Arya Taheri
 using GradeSheet;
 
-Console.WriteLine("Students File?: ");
-try
-{
-    string path = Console.ReadLine();
-    var s = new Semester(path);
+void executer(){
+    string path = null;
+    do
+    {
+        Console.WriteLine("Students File?: ");
+        path = Console.ReadLine();
+        if (path == null)
+        {
+            Console.WriteLine("bad input try again");
+        }
+    } while (path != null);
+
+    try
+    {
+        var s = new Semester(path);
+    }
+    catch {
+        executer();
+    }
 }
-catch
-{
-    Console.WriteLine("saggggggggg");
-}
-Console.WriteLine("jidfhjfidoh");
